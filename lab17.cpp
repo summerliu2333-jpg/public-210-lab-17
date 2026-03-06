704d392ebca9d3fd5cd195cd99a9fcaa353c63c7
Yang Liu <summerliu2333@gmail.com>
2026-03-05 17:01:32 -0800

    deleteNodeAtPosition

--- lab17.cpp
+++ lab17.cpp
@@ -47,6 +47,14 @@ int main() {
     cout << "Linked list after inserting 10000: " << endl;
     output(head);
 
+    // Delete node at specified position
+    cout << "Which node to delete?: ";
+    int del_pos;
+    cin >> del_pos;
+    head = deleteNodeAtPosition(head, del_pos);
+    cout << "Linked list after deleting node " << del_pos << ": " << endl;
+    output(head);
+
     return 0;
 }
 
@@ -108,5 +116,39 @@ Node* insertNodeAfterPosition(Node *head, int position, int val) {
     newnode->value = val;
     newnode->next = current;
     prev->next = newnode;
+    return head;
+}
+
+Node* deleteNodeAtPosition(Node *head, int position) {
+    if (!head || position < 1) {
+        cout << "Empty list or invalid position!" << endl;
+        return head;
+    }
+
+    Node *toDelete = nullptr;
+    // For delete head node
+    if (position == 1) {
+        toDelete = head;
+        head = head->next;
+        delete toDelete;
+        return head;
+    }
+
+    Node *prev = head;
+    int i = 1;
+    while (prev != nullptr && i < position - 1) {
+        prev = prev->next;
+        i++;
+    }
+
+    if (!prev || !prev->next) {
+        cout << "Position exceeds list length!" << endl;
+        return head;
+    }
+
+    toDelete = prev->next;
+    prev->next = toDelete->next;
+    delete toDelete;
+
     return head;
 }
